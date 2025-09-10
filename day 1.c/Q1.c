e29a855f6615b1ac35d6cb9959fb2f08efe3d2a7
mayankrj28-boop <mayankrj28@gmail.com>
2025-09-10 20:16:49 +0530

    Create Q1.c

---
+++ day 1.c/Q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a , b, sum ;
+
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    
+    sum = a + b;
+    printf("sum= %d\n", sum);
+
+    return 0;
+
+
+}
